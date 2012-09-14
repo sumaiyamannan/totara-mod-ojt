aaed83c74004596fe384b630a688487422f0c237
Eugene Venter <eugene@catalyst.net.nz>
2012-09-14 14:17:33 +1200

    login/index.php: noredirect/magicpony login
    Change-Id: I5dce63e1517ae3faa8886be2abe21d89cd47b58d

--- login/index.php
+++ login/index.php
@@ -38,6 +38,8 @@ redirect_if_major_upgrade_required();
 $testsession = optional_param('testsession', 0, PARAM_INT); // test session works properly
 $cancel      = optional_param('cancel', 0, PARAM_BOOL);      // redirect to frontpage, needed for loginhttps
 $anchor      = optional_param('anchor', '', PARAM_RAW);      // Used to restore hash anchor to wantsurl.
+$noredirect  = optional_param('magicpony', 0, PARAM_BOOL); // don't redirect
+$noredirect |= optional_param('noredirect', 0, PARAM_BOOL); // don't redirect
 
 if ($cancel) {
     redirect(new moodle_url('/'));
@@ -300,7 +302,7 @@ if (empty($SESSION->wantsurl)) {
 }
 
 /// Redirect to alternative login URL if needed
-if (!empty($CFG->alternateloginurl)) {
+if (!empty($CFG->alternateloginurl) && empty($noredirect)) {
     $loginurl = $CFG->alternateloginurl;
 
     if (strpos($SESSION->wantsurl, $loginurl) === 0) {
