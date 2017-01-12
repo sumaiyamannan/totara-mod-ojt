1380cbb66837694bfc83a8f701dd351ade2cbd1a
Riana Rossouw <riana.rossouw@totaralearning.com>
2017-01-12 14:19:53 +1300

    TL-11230 program: \totara_program\task\clean_enrolment_plugins_task only unsuspend users currently in the program's assignments
    Change-Id: I41438a6d60456e0a40601b436dda6a18a1896d8a
    Reviewed-on: https://review.totaralms.com/14269
    Reviewed-by: Brendan Cox <brendan.cox@totaralearning.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralearning.com>

--- totara/program/tests/generator/totara_program_generator.class.php
+++ totara/program/tests/generator/totara_program_generator.class.php
@@ -336,6 +336,81 @@ class totara_program_generator extends component_generator_base {
         $programcontent->save_content();
     }
 
+    /**
+     * Add courseset to program
+     *
+     * @param program $program
+     * @param int $coursesetnum  number of courseset
+     * @param stdClass[] $coursesetarray Array of courses to add to this course set.
+     */
+    public function add_courses_and_courseset_to_program(program $program, array $coursesetarray = array(), $certifpath = CERTIFPATH_CERT) {
+        global $CFG;
+        require_once($CFG->dirroot . '/totara/program/lib.php');
+        require_once($CFG->dirroot . '/totara/certification/lib.php');
+
+        $prefix = 1000;
+        $coursesetnum = 0;
+        $sortorder = 1;
+
+        $rawdata = new stdClass();
+        $rawdata->id = $program->id;
+        $rawdata->contentchanged = 1;
+        $rawdata->contenttype = 1;
+        $rawdata->setprefixes = [];
+        $rawdata->setprefixes_rc = [];
+
+        foreach ($coursesetarray as $courses) {
+
+            $coursesetnum ++;
+            $prefix --;
+            $sortorder ++;
+
+
+            $rawdata->setprefixes[] = $prefix;
+            $rawdata->setprefixes_rc[] = $prefix;
+
+            $courseids = array();
+            $coursesassigned = 0;
+            foreach ($courses as $course) {
+                $courseids[] = $course->id;
+                $coursesassigned++;
+            }
+
+            $rawdata->{$prefix.'courses'} = implode(',', $courseids);
+            $rawdata->{$prefix.'contenttype'} = 1;
+            $rawdata->{$prefix.'id'} = 0;
+            $rawdata->{$prefix.'label'} = "Course Set {$coursesetnum}";
+            $rawdata->{$prefix.'sortorder'} = $sortorder;
+            $rawdata->{$prefix.'contenttype'} = 1;
+            $rawdata->{$prefix.'nextsetoperator'} = '';
+            $rawdata->{$prefix.'completiontype'} = 1;
+            $rawdata->{$prefix.'timeallowedperiod'} = 2;
+            $rawdata->{$prefix.'timeallowednum'} = 1;
+
+            if ($certifpath === CERTIFPATH_RECERT) { // Re-certification path.
+                $rawdata->certifpath_rc = CERTIFPATH_RECERT;
+                $rawdata->iscertif = 1;
+                $rawdata->contenttype_rc = 1;
+                $rawdata->{$prefix.'certifpath'} = 2;
+                $rawdata->contenttype_rc = 1;
+            } else {
+                // Certification path.
+                $rawdata->certifpath_rc = CERTIFPATH_CERT;
+                $rawdata->iscertif = 0;
+                $rawdata->contenttype_rc = 1;
+                $rawdata->{$prefix.'certifpath'} = 1;
+                $rawdata->contenttype_rc = 1;
+            }
+        }
+
+        $rawdata->setprefixes = join(',', $rawdata->setprefixes);
+        $rawdata->setprefixes_rc = join(',', $rawdata->setprefixes_rc);
+
+        $programcontent = $program->get_content();
+        $programcontent->setup_content($rawdata);
+        $programcontent->save_content();
+    }
+
     private function complete_course($courseid, $userid) {
         $comp_man = new stdClass();
         $comp_man->id = $DB->get_field('course_completions', 'id',
