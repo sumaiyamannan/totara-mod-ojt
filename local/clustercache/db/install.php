c2141644523c865a8f224a3b5fe9cc70c7249f60
Pierre Guinoiseau <pierre.guinoiseau@catalyst.net.nz>
2017-03-21 14:43:49 +1300

    local/clustercache: avoid errors on install when the memcache module is not installed

--- local/clustercache/db/install.php
+++ local/clustercache/db/install.php
@@ -6,5 +6,9 @@ require_once($CFG->dirroot.'/cache/forms.php');
 require_once($CFG->dirroot.'/local/clustercache/locallib.php');
 
 function xmldb_local_clustercache_install() {
+    // Return early if the memcache module is not installed
+    if (!class_exists('Memcache')) {
+        return true;
+    }
     return clustercache_setup_cache();
 }
