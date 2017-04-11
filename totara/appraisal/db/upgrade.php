3917dd69fb0482ff9b0f944a6916985a56482983
Simon Player <simon.player@totaralearning.com>
2017-04-11 17:18:17 +0100

    TL-12803 tasks: set default scheduled tasks correctly
    Change-Id: Ieca6354007b8ec9dc6c81b54f3ae7880d3ac82c6
    Reviewed-on: https://review.totaralms.com/14991
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralearning.com>
    Reviewed-by: Maurice Moore <maurice.moore@totaralearning.com>

--- totara/appraisal/db/upgrade.php
+++ totara/appraisal/db/upgrade.php
@@ -335,5 +335,26 @@ function xmldb_totara_appraisal_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2016041800, 'totara', 'appraisal');
     }
 
+    // Set default scheduled tasks correctly.
+    if ($oldversion < 2016092001) {
+
+        $task = '\totara_appraisal\task\cleanup_task';
+        // If schecdule is * 3 * * * change to 0 3 * * *
+        $incorrectschedule = array(
+            'minute' => '*',
+            'hour' => '3',
+            'day' => '*',
+            'month' => '*',
+            'dayofweek' => '*'
+        );
+        $newschedule = $incorrectschedule;
+        $newschedule['minute'] = '0';
+
+        totara_upgrade_default_schedule($task, $incorrectschedule, $newschedule);
+
+        // Appraisal savepoint reached.
+        upgrade_plugin_savepoint(true, 2016092001, 'totara', 'appraisal');
+    }
+
     return true;
 }
