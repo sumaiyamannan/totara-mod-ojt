73924ab48db527b0e96d4b4b0a33d2b1b924e6c0
Eugene Venter <eugene@catalyst.net.nz>
2017-11-29 12:11:32 +1300

    rb_source_ojt_completion.php: update basic user content options

--- rb_sources/rb_source_ojt_completion.php
+++ rb_sources/rb_source_ojt_completion.php
@@ -299,36 +299,13 @@ class rb_source_ojt_completion extends rb_base_source {
     }
 
     protected function define_contentoptions() {
-        $contentoptions = array(
-            new rb_content_option(
-                'current_pos',
-                get_string('currentpos', 'totara_reportbuilder'),
-                'position.path',
-                'position'
-            ),
-            new rb_content_option(
-                'current_org',
-                get_string('currentorg', 'totara_reportbuilder'),
-                'organisation.path',
-                'organisation'
-            ),
-            new rb_content_option(
-                'user',
-                get_string('user', 'rb_source_ojt_completion'),
-                array(
-                    'userid' => 'base.userid',
-                    'managerid' => 'position_assignment.managerid',
-                    'managerpath' => 'position_assignment.managerpath',
-                    'postype' => 'position_assignment.type',
-                ),
-                'position_assignment'
-            ),
-            new rb_content_option(
-                'ojt_completion_type',
-                get_string('ojtcompletiontype', 'rb_source_ojt_completion'),
-                'base.type',
-                'base'
-            ),
+        $contentoptions = array();
+        $this->add_basic_user_content_options($contentoptions);
+        $contentoptions[] = new rb_content_option(
+            'ojt_completion_type',
+            get_string('ojtcompletiontype', 'rb_source_ojt_completion'),
+            'base.type',
+            'base'
         );
         return $contentoptions;
     }
