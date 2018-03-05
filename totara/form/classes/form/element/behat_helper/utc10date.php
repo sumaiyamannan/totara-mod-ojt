6231c97b02d2482346c5fa9ff5c95aad7e48e7ee
Brian Barnes <brian.barnes@totaralms.com>
2018-03-05 15:31:58 +1300

    TL-16308 totara_form: Behat now hides datepicker after date is set

--- totara/form/classes/form/element/behat_helper/utc10date.php
+++ totara/form/classes/form/element/behat_helper/utc10date.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of Totara LMS
  *
- * Copyright (C) 2016 onwards Totara Learning Solutions LTD
+ * Copyright (C) 2017 onwards Totara Learning Solutions LTD
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author Sam Hemelryk <sam.hemelryk@totaralearning.com>
+ * @author Petr Skoda <petr.skoda@totaralearning.com>
  * @package totara_form
  */
 
@@ -25,17 +25,17 @@ namespace totara_form\form\element\behat_helper;
 use Behat\Mink\Exception\ExpectationException;
 
 /**
- * A datetime element helper.
+ * A utc10date element helper.
  *
- * @author Sam Hemelryk <sam.hemelryk@totaralearning.com>
- * @copyright 2016 Totara Learning Solutions Ltd {@link http://www.totaralms.com/}
+ * @author Petr Skoda <petr.skoda@totaralearning.com>
+ * @copyright 2017 Totara Learning Solutions Ltd {@link http://www.totaralms.com/}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @package totara_form
  */
-class datetime extends text {
+class utc10date extends text {
 
     /**
-     * Sets the value of the datetime input.
+     * Sets the value of the utc10date input.
      *
      * @param string $value
      * @throws \Behat\Mink\Exception\ExpectationException
@@ -50,13 +50,16 @@ class datetime extends text {
         // If JS is running then we need to use JS to set the value.
         // It has to be perfectly formatted.
         $text = $this->get_text_input();
+        if ($this->context->running_javascript() && !$text->isVisible()) {
+            throw new ExpectationException('Attempting to change a ' . $this->mytype . ' that is not visible', $this->context->getSession());
+        }
         $id = $this->node->getAttribute('data-element-id');
         $js  = 'var e, t;';
         $js .= 'e = document.getElementById(' . json_encode($id) . ');';
         $js .= 'e.value = ' . json_encode($value) . ';';
         $this->context->getSession()->executeScript($js);
 
-        // As this value is set via Javascript, simulate the change event
+        // Trigger the onchange event as triggered when selecting a real value.
         $this->context->getSession()->getDriver()->triggerSynScript(
             $text->getXPath(),
             "Syn.trigger('change', {}, {{ELEMENT}})"
@@ -76,27 +79,8 @@ class datetime extends text {
     protected function get_value() {
         $value = parent::get_value();
         // Remove the trailing seconds.
-        if (preg_match('/\d{1,2}:\d{1,2}:00$/', $value)) {
-            $value = substr($value, 0, -3);
-        }
+        $value = preg_replace('/\d{1,2}:\d{1,2}(:\d{1,2})?$/', '', $value);
         return $value;
-    }
-
-    /**
-     * Asserts the field has expected value.
-     *
-     * @param string $expectedvalue
-     * @return void
-     */
-    public function assert_value($expectedvalue) {
-        $value = self::normalise_value_pre_set($this->get_value());
-        $expected = self::normalise_value_pre_set($expectedvalue);
-
-        if ($expected === $value) {
-            return;
-        }
-
-        throw new ExpectationException("Totara form {$this->mytype} element '{$this->locator}' does not match expected value: {$expectedvalue}", $this->context->getSession());
     }
 
     /**
@@ -108,14 +92,12 @@ class datetime extends text {
     public static function normalise_value_pre_set($value) {
         $value = trim($value);
 
-        if ($value === '') {
+        if ($value === '' or $value === '0') {
             return '';
         }
 
-        // Alright in behat feature files we accept a couple of different formats.
-        // 1. YYYY-MM-DD(T| )hh:mm - this is the default
-        // 2. YYYY-MM-DD  - this is our own.
-        $regexdate = '#^(?P<year>\d{2,4})[\-/ ](?P<month>\d{1,2})[\-/ ](?P<day>\d{1,2})([T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2}))?$#';
+        // Only YYYY-MM-DD and variation with spaces or slashes are accepted.
+        $regexdate = '#^(?P<year>\d{2,4})[\-/ ](?P<month>\d{1,2})[\-/ ](?P<day>\d{1,2})$#';
         // The internal must start with +P or -P.
         $regexinterval = '/^([+-])(P(\d+Y)?(\d+M)?(\d+W)?(\d+D)?(T(\d+H)?(\d+M)?(\d+S)?)?)$/';
 
@@ -129,24 +111,38 @@ class datetime extends text {
             }
 
         } else if (preg_match($regexdate, $value, $matches)) {
-            $year = $matches['year'];
+            $year = (int)$matches['year'];
             $month = (int)$matches['month'];
             $day = (int)$matches['day'];
-            // Why 3:45pm you ask - just so that we know what we can expect when the datetime has not been specified.
-            $hour = isset($matches['hour']) ? (int)$matches['hour'] : 15;
-            $minute = isset($matches['minute']) ? (int)$matches['minute'] : 45;
+
             if ($year < 99) {
                 $year += 2000;
             }
 
             $date = new \DateTime();
             $date->setDate($year, $month, $day);
-            $date->setTime($hour, $minute, 0);
 
         } else {
-            throw new \coding_exception('Invalid datetime value provided, it should be YYYY-MM-DD hh:mm date or relative +/- P interval, "'.$value.'"');
+            throw new \coding_exception('Invalid utc10date value provided, it should be YYYY-MM-DD date or relative +/- P interval, "'.$value.'"');
         }
 
-        return $date->format('Y-m-d\TH:i');
+        return $date->format('Y-m-d');
+    }
+
+    /**
+     * Asserts the field has expected value.
+     *
+     * @param string $expectedvalue
+     * @return void
+     */
+    public function assert_value($expectedvalue) {
+        $value = self::normalise_value_pre_set($this->get_value());
+        $expected = self::normalise_value_pre_set($expectedvalue);
+
+        if ($expected == $value) {
+            return;
+        }
+
+        throw new ExpectationException("Totara form {$this->mytype} element '{$this->locator}' does not match expected value: {$expectedvalue}", $this->context->getSession());
     }
 }
