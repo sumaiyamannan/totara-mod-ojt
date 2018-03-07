397345dee603eaa6c7e211ee49734a055e813720
Eugene Venter <eugene@catalyst.net.nz>
2018-03-07 14:35:12 +1300

    mod/ojt: add topic item completion report source

---
+++ rb_sources/rb_source_ojt_topic_item_completion.php
@@ -0,0 +1,403 @@
+<?php
+/*
+ * This file is part of Totara LMS
+ *
+ * Copyright (C) 2010 onwards Totara Learning Solutions LTD
+ * Copyright (C) 1999 onwards Martin Dougiamas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Eugene Venter <eugene@catalyst.net.nz>
+ * @package mod
+ * @subpackage ojt
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class rb_source_ojt_topic_item_completion extends rb_base_source {
+    public $base, $joinlist, $columnoptions, $filteroptions;
+    public $contentoptions, $paramoptions, $defaultcolumns;
+    public $defaultfilters, $requiredcolumns, $sourcetitle;
+
+    function __construct() {
+        global $CFG, $DB;
+        require_once($CFG->dirroot.'/mod/ojt/lib.php');
+
+        $this->base = '{ojt_completion}';
+        $this->joinlist = $this->define_joinlist();
+        $this->columnoptions = $this->define_columnoptions();
+        $this->filteroptions = $this->define_filteroptions();
+        $this->contentoptions = $this->define_contentoptions();
+        $this->paramoptions = $this->define_paramoptions();
+        $this->defaultcolumns = $this->define_defaultcolumns();
+        $this->defaultfilters = $this->define_defaultfilters();
+        $this->requiredcolumns = $this->define_requiredcolumns();
+        $this->sourcetitle = get_string('ojttopicitemcompletion', 'rb_source_ojt_topic_item_completion');
+        $this->sourcewhere = 'base.type = '.OJT_CTYPE_TOPICITEM;
+
+        parent::__construct();
+    }
+
+    //
+    //
+    // Methods for defining contents of source
+    //
+    //
+
+    protected function define_joinlist() {
+        global $CFG;
+
+        // to get access to constants
+        require_once($CFG->dirroot.'/mod/ojt/lib.php');
+
+        $joinlist = array(
+            new rb_join(
+                'ojt',
+                'LEFT',
+                '{ojt}',
+                'base.ojtid = ojt.id',
+                REPORT_BUILDER_RELATION_ONE_TO_ONE
+            ),
+            new rb_join(
+                'ojt_topic',
+                'LEFT',
+                '{ojt_topic}',
+                'base.topicid = ojt_topic.id',
+                REPORT_BUILDER_RELATION_ONE_TO_ONE
+            ),
+            new rb_join(
+                'ojt_topic_item',
+                'LEFT',
+                '{ojt_topic_item}',
+                'base.topicitemid = ojt_topic_item.id',
+                REPORT_BUILDER_RELATION_ONE_TO_ONE
+            ),
+            new rb_join(
+                'modifyuser',
+                'LEFT',
+                '{user}',
+                'base.modifiedby = modifyuser.id',
+                REPORT_BUILDER_RELATION_ONE_TO_ONE
+            ),
+        );
+
+        // include some standard joins
+        $this->add_user_table_to_joinlist($joinlist, 'base', 'userid');
+        $this->add_course_table_to_joinlist($joinlist, 'ojt', 'course');
+        // requires the course join
+        $this->add_course_category_table_to_joinlist($joinlist,
+            'course', 'category');
+        $this->add_job_assignment_tables_to_joinlist($joinlist, 'base', 'userid');
+        $this->add_core_tag_tables_to_joinlist('core', 'course', $joinlist, 'ojt', 'course');
+        $this->add_cohort_course_tables_to_joinlist($joinlist, 'ojt', 'course');
+
+        return $joinlist;
+    }
+
+    protected function define_columnoptions() {
+        global $DB;
+
+        $columnoptions = array(
+            new rb_column_option(
+                'ojt',
+                'name',
+                get_string('ojt', 'rb_source_ojt_topic_item_completion'),
+                'ojt.name',
+                array('joins' => 'ojt', 'displayfunc' => 'ojt_link',
+                    'extrafields' => array('userid' => 'base.userid', 'ojtid' => 'base.ojtid'))
+            ),
+            new rb_column_option(
+                'ojt',
+                'evaluatelink',
+                get_string('evaluatelink', 'rb_source_ojt_topic_item_completion'),
+                'ojt.name',
+                array('joins' => 'ojt', 'displayfunc' => 'ojt_evaluate_link',
+                    'extrafields' => array('userid' => 'base.userid', 'ojtid' => 'base.ojtid'))
+            ),
+            new rb_column_option(
+                'ojt_topic',
+                'name',
+                get_string('topic', 'rb_source_ojt_topic_item_completion'),
+                'ojt_topic.name',
+                array('joins' => 'ojt_topic')
+            ),
+            new rb_column_option(
+                'ojt_topic_item',
+                'name',
+                get_string('topicitem', 'rb_source_ojt_topic_item_completion'),
+                'ojt_topic_item.name',
+                array('joins' => 'ojt_topic_item')
+            ),
+            new rb_column_option(
+                'base',
+                'status',
+                get_string('completionstatus', 'rb_source_ojt_topic_item_completion'),
+                'base.status',
+                array('displayfunc' => 'ojt_completion_status')
+            ),
+            new rb_column_option(
+                'base',
+                'timemodified',
+                get_string('timemodified', 'rb_source_ojt_topic_item_completion'),
+                'base.timemodified',
+                array('displayfunc' => 'nice_datetime')
+            ),
+            new rb_column_option(
+                'base',
+                'modifiedby',
+                get_string('modifiedby', 'rb_source_ojt_topic_item_completion'),
+                $DB->sql_fullname("modifyuser.firstname", "modifyuser.lastname"),
+                array(
+                    'joins' => 'modifyuser',
+                    'displayfunc' => 'link_user',
+                    'extrafields' => array('user_id' => "modifyuser.id"),
+                )
+            ),
+            new rb_column_option(
+                'base',
+                'comment',
+                get_string('comment', 'rb_source_ojt_topic_item_completion'),
+                'base.comment'
+            ),
+
+        );
+
+        // include some standard columns
+        $this->add_user_fields_to_columns($columnoptions);
+        $this->add_course_fields_to_columns($columnoptions);
+        $this->add_course_category_fields_to_columns($columnoptions);
+        $this->add_job_assignment_fields_to_columns($columnoptions);
+        $this->add_core_tag_fields_to_columns('core', 'course', $columnoptions);
+        $this->add_cohort_course_fields_to_columns($columnoptions);
+
+        return $columnoptions;
+    }
+
+    protected function define_filteroptions() {
+        $filteroptions = array(
+
+            new rb_filter_option(
+                'ojt',
+                'name',
+                get_string('ojtname', 'rb_source_ojt_topic_item_completion'),
+                'text'
+            ),
+            new rb_filter_option(
+                'ojt_topic',
+                'name',
+                get_string('topicname', 'rb_source_ojt_topic_item_completion'),
+                'text'
+            ),
+            new rb_filter_option(
+                'ojt_topic_item',
+                'name',
+                get_string('topicitemname', 'rb_source_ojt_topic_item_completion'),
+                'text'
+            ),
+            new rb_filter_option(
+                'base',
+                'comment',
+                get_string('comment', 'rb_source_ojt_topic_item_completion'),
+                'text'
+            ),
+            new rb_filter_option(
+                'base',
+                'timemodified',
+                get_string('timemodified', 'rb_source_ojt_topic_item_completion'),
+                'date'
+            ),
+            new rb_filter_option(
+                'base',
+                'status',
+                get_string('completionstatus', 'rb_source_ojt_topic_item_completion'),
+                'select',
+                array(
+                    'selectfunc' => 'ojt_completion_status_list',
+                )
+            ),
+        );
+
+        // include some standard filters
+        $this->add_user_fields_to_filters($filteroptions);
+        $this->add_course_fields_to_filters($filteroptions);
+        $this->add_course_category_fields_to_filters($filteroptions);
+        $this->add_job_assignment_fields_to_filters($columnoptions);
+        $this->add_core_tag_fields_to_filters('core', 'course', $filteroptions);
+        $this->add_cohort_course_fields_to_filters($filteroptions);
+
+        return $filteroptions;
+    }
+
+    protected function define_contentoptions() {
+        $contentoptions = array(
+            new rb_content_option(
+                'current_pos',
+                get_string('currentpos', 'totara_reportbuilder'),
+                'position.path',
+                'position'
+            ),
+            new rb_content_option(
+                'current_org',
+                get_string('currentorg', 'totara_reportbuilder'),
+                'organisation.path',
+                'organisation'
+            ),
+            new rb_content_option(
+                'user',
+                get_string('user', 'rb_source_ojt_topic_item_completion'),
+                array(
+                    'userid' => 'base.userid',
+                    'managerid' => 'position_assignment.managerid',
+                    'managerpath' => 'position_assignment.managerpath',
+                    'postype' => 'position_assignment.type',
+                ),
+                'position_assignment'
+            ),
+        );
+        return $contentoptions;
+    }
+
+    protected function define_paramoptions() {
+        $paramoptions = array();
+
+        return $paramoptions;
+    }
+
+    protected function define_defaultcolumns() {
+        $defaultcolumns = array(
+            array(
+                'type' => 'user',
+                'value' => 'namelink',
+            ),
+            array(
+                'type' => 'course',
+                'value' => 'courselink',
+            ),
+            array(
+                'type' => 'ojt',
+                'value' => 'name',
+            ),
+            array(
+                'type' => 'ojt_topic',
+                'value' => 'name',
+            ),
+            array(
+                'type' => 'ojt_topic_item',
+                'value' => 'name',
+            ),
+            array(
+                'type' => 'base',
+                'value' => 'status',
+            ),
+            array(
+                'type' => 'base',
+                'value' => 'comment',
+            ),
+
+        );
+        return $defaultcolumns;
+    }
+
+    protected function define_defaultfilters() {
+        $defaultfilters = array(
+            array(
+                'type' => 'ojt',
+                'value' => 'name',
+            ),
+            array(
+                'type' => 'ojt_topic',
+                'value' => 'name',
+            ),
+            array(
+                'type' => 'ojt_topic_item',
+                'value' => 'name',
+            ),
+            array(
+                'type' => 'base',
+                'value' => 'status',
+            ),
+            array(
+                'type' => 'user',
+                'value' => 'fullname',
+            ),
+            array(
+                'type' => 'course',
+                'value' => 'fullname',
+                'advanced' => 1,
+            ),
+        );
+
+        return $defaultfilters;
+    }
+
+    protected function define_requiredcolumns() {
+        $requiredcolumns = array(
+            new rb_column(
+                'ojt_topic_item',
+                'id',
+                '',
+                'ojt_topic_item.id',
+                array('joins' => 'ojt_topic_item')
+            ),
+		);
+
+        return $requiredcolumns;
+    }
+
+    //
+    //
+    // Source specific column display methods
+    //
+    //
+
+    function rb_display_ojt_completion_status($status, $row, $isexport) {
+        if (empty($status)) {
+            return get_string('completionstatus'.OJT_INCOMPLETE, 'ojt');
+        } else {
+            return get_string('completionstatus'.$status, 'ojt');
+        }
+    }
+
+    function rb_display_ojt_link($ojtname, $row, $isexport) {
+        return html_writer::link(new moodle_url('/mod/ojt/evaluate.php',
+            array('userid' => $row->userid, 'bid' => $row->ojtid)), $ojtname);
+
+    }
+
+    function rb_display_ojt_evaluate_link($ojtname, $row, $isexport) {
+        return html_writer::link(new moodle_url('/mod/ojt/evaluate.php',
+            array('userid' => $row->userid, 'bid' => $row->ojtid)), get_string('evaluate', 'rb_source_ojt_topic_item_completion'));
+
+    }
+
+
+
+    //
+    //
+    // Source specific filter display methods
+    //
+    //
+
+    function rb_filter_ojt_completion_status_list() {
+        $statuses = array(OJT_INCOMPLETE, OJT_REQUIREDCOMPLETE, OJT_COMPLETE);
+        $statuslist = array();
+        foreach ($statuses as $status) {
+            $statuslist[$status] = get_string('completionstatus'.$status, 'ojt');
+        }
+
+        return $statuslist;
+    }
+
+} // class
+
