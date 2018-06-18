09797d6ebff1632841f8f4d17f20c55e9685f67c
David Curry <david.curry@totaralearning.com>
2018-06-19 09:53:01 +1200

    TL-17783 version.php: Tagging version 9.21

--- version.php
+++ version.php
@@ -46,6 +46,6 @@ $maturity = MATURITY_STABLE;            // this version's maturity level
 
 $TOTARA = new stdClass();
 
-$TOTARA->version    = '9.20+';          // Please keep as string.
-$TOTARA->build      = '20180514.00';    // Please keep as string.
+$TOTARA->version    = '9.21';          // Please keep as string.
+$TOTARA->build      = '20180620.00';    // Please keep as string.
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
