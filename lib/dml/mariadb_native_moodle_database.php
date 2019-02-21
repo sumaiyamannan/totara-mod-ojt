e49864ceff7a182da0e2814dedda251eaec87234
Yuliya Bozhko <yuliya.bozhko@totaralearning.com>
2019-02-21 17:16:36 +0000

    TL-20033 dml: Fix word boundary matching in MySQL 8

--- lib/dml/mariadb_native_moodle_database.php
+++ lib/dml/mariadb_native_moodle_database.php
@@ -82,6 +82,28 @@ class mariadb_native_moodle_database extends mysqli_native_moodle_database {
     }
 
     /**
+     * Returns the driver specific syntax for the beginning of a word boundary.
+     *
+     * @since Totara 9.30
+     * @return string or empty if not supported
+     */
+    public function sql_regex_word_boundary_start() {
+        // MariaDB doesn't have regexp issue specific to MySQL, so override it.
+        return '[[:<:]]';
+    }
+
+    /**
+     * Returns the driver specific syntax for the end of a word boundary.
+     *
+     * @since Totara 9.30
+     * @return string or empty if not supported
+     */
+    public function sql_regex_word_boundary_end() {
+        // MariaDB doesn't have regexp issue specific to MySQL, so override it.
+        return '[[:>:]]';
+    }
+
+    /**
      * Returns database server info array
      * @return array Array containing 'description' and 'version' info
      */
