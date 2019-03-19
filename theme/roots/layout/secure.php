edd8b8d9fb15b70b3f282c58d1bd1957529dcc26
Brian Barnes <brian.barnes@totaralms.com>
2019-03-19 15:47:27 +1300

    TL-20518 theme_roots: corrected use of secure page layout

--- theme/roots/layout/secure.php
+++ theme/roots/layout/secure.php
@@ -87,7 +87,6 @@ echo $OUTPUT->doctype() ?>
 <div id="page" class="container-fluid">
     <header id="page-header" class="clearfix">
         <div id="page-navbar" class="clearfix">
-            <nav class="breadcrumb-nav" role="navigation" aria-label="breadcrumb"><?php echo $OUTPUT->navbar(); ?></nav>
             <div class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></div>
         </div>
 
