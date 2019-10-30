f5ff4ef5b63a24c35eb5e42e9a0e88088ba924bb
Petr Skoda <petr.skoda@totaralearning.com>
2019-10-31 10:52:52 +1300

    TL-22886 login: remove invalid password length restrictions

--- login/signup_form.php
+++ login/signup_form.php
@@ -48,7 +48,7 @@ class login_signup_form extends moodleform {
         if (!empty($CFG->passwordpolicy)){
             $mform->addElement('static', 'passwordpolicyinfo', '', print_password_policy());
         }
-        $mform->addElement('passwordunmask', 'password', get_string('password'), 'maxlength="32" size="12"');
+        $mform->addElement('passwordunmask', 'password', get_string('password'), 'size="12"');
         $mform->setType('password', PARAM_RAW);
         $mform->addRule('password', get_string('missingpassword'), 'required', null, 'client');
 
