4ac1a8a57be887b06014583eabf479214649124a
David Thompson <david.thompson@catalyst.net.nz>
2020-05-23 16:56:21 +1200

    Added mod/reengagement (2016082302).
    WR#333652

---
+++ mod/reengagement/pix/flex_icons.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * File description
+ *
+ * @package
+ * @subpackage
+ * @copyright  &copy; 2017 Kineo Pacific {@link http://kineo.com.au}
+ * @author     tri.le
+ * @version    1.0
+ */
+
+$icons = array(
+    'mod_reengagement|icon' =>
+        array(
+            'data' =>
+                array(
+                    'classes' => 'fa-clock-o',
+                ),
+        ),
+);
