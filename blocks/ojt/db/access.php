0a245b014126b3cfeab174a8e4763f830e62bc8e
David Thompson <david.thompson@catalyst.net.nz>
2020-05-26 15:55:20 +1200

    Adding block_ojt from Kineo.
    WR#333652

---
+++ blocks/ojt/db/access.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+ * Comment
+ *
+ * @package    block
+ * @subpackage ojt
+ * @copyright  &copy; 2017 CG Kineo {@link http://www.kineo.com}
+ * @author     kaushtuv.gurung
+ * @version    1.0
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'block/ojt:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+        
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+    
+    'block/ojt:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    )
+);
